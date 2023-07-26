fdfcf0593b510996e9a7a8c245cf4c90f60ab5b0
HoVanDi <hovandideveloper@gmail.com>
2023-07-26 17:06:48 +0700

    finish

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,37 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Lấy tất cả các thông báo chưa đọc
-  const unreadNotifications = document.querySelectorAll(".comment.comment-read");
+  const unreadNotifications = document.querySelectorAll(".comment");
+  const notificationNumber = document.querySelector(".notification-number");
+  const readAllNotifications = document.querySelector(".notification-btn");
 
-  // Gán sự kiện nhấp chuột cho từng thông báo
+  // Assign click event to each notification
   unreadNotifications.forEach(function (notification) {
-    // Lấy đối tượng notification-icon bên trong thông báo
     const notificationIcon = notification.querySelector(".notification-icon");
 
-    // Gán sự kiện click cho cả thông báo (bao gồm cả icon)
+    // Assign click event to all notifications (including icons)
     notification.addEventListener("click", function () {
-      // Kiểm tra xem thông báo có class unread hay không
-      if (notification.classList.contains("unread")) {
-        // Khi một thông báo được nhấp vào,
-        // đổi màu nền và ẩn icon
-        notification.classList.remove("unread");
-        notification.classList.add("read");
+      notification.classList.add("read");
+      notificationIcon.style.display = "none";
 
-        // Ẩn phần tử icon trong thông báo nếu tồn tại
-        if (notificationIcon) {
-          notificationIcon.style.display = "none";
-        }
-
-        // Giảm số lượng thông báo
-        const notificationNumber = document.querySelector(".notification-number");
-        let unreadCount = parseInt(notificationNumber.textContent);
-        unreadCount--;
-        notificationNumber.textContent = unreadCount;
-
-        // Nếu số lượng thông báo đã đọc là 0, ẩn nút thông báo
-        if (unreadCount === 0) {
-          notificationNumber.style.display = "none";
-        }
+      const currentNumber = parseInt(notificationNumber.textContent);
+      if (currentNumber >= 1) {
+        notificationNumber.textContent = currentNumber - 1;
       }
     });
+
+  });
+  // Assign click event to "Mark all as read" button
+  readAllNotifications.addEventListener("click", function () {
+    unreadNotifications.forEach(function (notification) {
+      notification.classList.add("read");
+      const notificationIcon = notification.querySelector(".notification-icon");
+      if (notificationIcon) {
+        notificationIcon.style.display = "none";
+      }
+    });
+    notificationNumber.textContent = "0";
   });
 });
